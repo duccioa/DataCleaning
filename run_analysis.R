9ce7e2b8dd6c477ec8e8fc5afc715eb70b186add
duccioa <duccio.aiazzi@gmail.com>
2015-01-25 19:24:43 +0100

    Final commitment

--- run_analysis.R
+++ run_analysis.R
@@ -1,34 +1,66 @@
-#path is the name of a directory in the working directory containing all the data files
-#the function creates one object ("data frames tbl" from the dplyr package) from every file in the directory and with the same name minus the extension
-#so that it can be easly analysed and compared with the other files
-# call createObjects("all_data") from the folder 
-readData <- function(){
-      obj_names <- c("X_train", "X_test", "y_train", "y_test", "subject_train", "subject_test","features")
-      full_list <- list.files(path = ".", full.names = TRUE, recursive = TRUE)
-      for(i in 1:length(obj_names)) {
-            path <- grep(paste("/", obj_names[i], ".txt",sep = ""), full_list, value = TRUE)
-            assign(obj_names[i], data.frame(read.table(path)), envir = .GlobalEnv)
+#REQUIREMENTS
+#The code run_analysis.R requires the package dplyr to run correctly. Dplyr is called from the library at the beginning of the script, if not present in the library, please install it before running it.
+#The dataset Human Activity Recognition Using Smartphones Dataset V1.0 has to be downloaded and unzipped in the working directory.
+#INSTRUCTIONS
+#The main function to be run is tidyDataset(), which returns a tidy data set with the average of each variable for each activity and each subject. 
+#The functions readData, mergeData, cleanMemory and renameActivities are called during the process by the main function
+#The steps of the manipulation described in the README.md file are annotated next to the relevant code line
+
+
+tidyDataset <- function(final_output = "tidyDataset") {
+      library("dplyr")
+      readData()
+      mergeData() #STEP 1 Merge the train and test sets to create one dataset.
+      clearMemory()
+      
+      DF2 <- DF1[, grep("mean|std", features$V2)] #STEP 2 - data frame with variables with mean and std only (from the main data frame, take only the rows resulting from grep on the feature file)
+      DF2 <- cbind(Y, subjects, DF2) #merge with the activity vector
+      renameActivities(DF2, "DF3") #STEP 3 - descriptive activity names to name the activities in the data set
+      #STEP 4 - ppropriately label the data set with descriptive variable names. 
+      features <- make.names(features$V2, unique = TRUE) #make the variable names legal(no brackets, no "-", etc.)
+      colnames(DF3)[3:ncol(DF3)] <- grep("mean|std", features, value = TRUE)#rename the columns with the names from the vector "features" which only contains "mean" or "std"
+      
+      #STEP 5 - Return as an output an independent tidy data set with the average of each variable for each activity and each subject.   
+      DF_group <- group_by(DF3, Subjects, Activities)#the dataframe is grouped by the activities and subject
+      assign(final_output, summarise_each(DF_group, funs(mean)), envir = .GlobalEnv)#the output dataframe summarises for each pair subject/activity the means of each variable
+      
+      #remove temporary variables
+      rm("features", "subjects", "Y", "DF1", "DF3",envir = .GlobalEnv)
+      
+      
+}
+
+
+
+readData <- function(){ #read the dataset and creates the objects for the manipulation from the relevant files
+      obj_names <- c("X_train", "X_test", "y_train", "y_test", "subject_train", "subject_test","features") #name of the relevant files for the manipulation
+      full_list <- list.files(path = ".", full.names = TRUE, recursive = TRUE) # list of all the files of the working directory
+      for(i in 1:length(obj_names)) { #extract the relevant files and assign the to objects
+            path <- grep(paste("/", obj_names[i], ".txt",sep = ""), full_list, value = TRUE)#extract from the complete list of files, only the files with the relevant name
+            assign(obj_names[i], data.frame(read.table(path)), envir = .GlobalEnv)#assign to each relevant name an object 
       }
 }
 
-mergeData <- function(){
-      #as required by the assignment, we merge the train and test datasets
-      DF1 <<- rbind(X_train, X_test) #QUESTION 1
-      Y <<- rbind(y_train, y_test)
-      colnames(Y) <<- c("Activities")
+mergeData <- function(){ #The train and test sets are merged in one dataset
+      DF1 <<- rbind(X_train, X_test) 
+      Y <<- rbind(y_train, y_test) 
+      colnames(Y) <<- c("Activities") 
       subjects <<- rbind(subject_train, subject_test)
       colnames(subjects) <<- c("Subjects")
       
 }
-clearMemory <- function(){
-      rm(X_test, X_train, y_test, y_train, subject_test, subject_train, envir = .GlobalEnv)#clean the memory for unused objects
+clearMemory <- function(){#function called to remove from the memory some working variables when they become obsolete
+      rm(X_test, X_train, y_test, y_train, subject_test, subject_train, envir = .GlobalEnv)
 }
-#renameActivities take the name of the data frame as an input and a name in quotation mark as output - ex. renameActivities(DF, "DF2")
+
+
+
+#renameActivities takes the name of the data frame as an input and a name in quotation mark as an output - ex. renameActivities(DF, "DF2")
 renameActivities <- function(data, output) {
       
       data$Activities <- as.character(data$Activities)
-      activities_names <-c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING")
-      for(i in 1:length(data$Activities)) {
+      activities_names <-c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING")#Activitiy names from the file activity_labels.txt
+      for(i in 1:length(data$Activities)) {#each line of the variable Activities is checked and replaced with the equivalent activity name
             k <- as.integer(data$Activities[i])
             data$Activities[i] <- activities_names[k]
             
@@ -38,29 +70,7 @@ renameActivities <- function(data, output) {
 
 
 
-tidyDataset <- function(final_output = "tidyDataset") {
-      
-      readData()
-      mergeData() #QUESTION 1 - 
-      clearMemory()
-      
-      DF_mean_std <- DF1[, grep("mean|std", features$V2)] #QUESTION 2 - data frame with variables with mean and std only (from the main data frame, take only the rows resulting from grep on the feature file)
-      DF2 <<- DF_mean_std
-      DF_mean_std <- cbind(Y, subjects, DF_mean_std) #merge with the activities vector
-      renameActivities(DF_mean_std, "DF3") #QUESTION 3 - descriptive activity names to name the activities in the data set
-      #QUESTION 4
-      features <- make.names(features$V2, unique = TRUE) #make the variable names legal(no brackets, no "-", etc.)
-      DF4 <- DF3
-      colnames(DF4)[3:ncol(DF4)] <- grep("mean|std", features, value = TRUE)#rename the columns with the names from the vector "features" which contain "mean" or "std"
-      DF4 <<- DF4
-      #QUESTION 5
-      
-      DF_group <- group_by(DF4, Subjects, Activities)
-      assign(final_output, summarise_each(DF_group, funs(mean)), envir = .GlobalEnv)
-      rm("features", "subjects", "Y", "DF1", "DF2", "DF3","DF4",envir = .GlobalEnv)
-      
-      
-}
 
 
+tidyDataset()
 
